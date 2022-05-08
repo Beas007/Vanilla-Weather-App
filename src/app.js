385f90dec4a3933497c9e7708de63a8f1fd39389
Beas007 <98432650+Beas007@users.noreply.github.com>
2022-05-08 23:26:17 +0100

    updated temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
+function displayTemprature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#showCity");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let highElement = document.querySelector("#high");
+  let lowElement = document.querySelector("#low");
+  let feelsElement = document.querySelector("#feels");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  highElement.innerHTML = Math.round(response.data.main.temp_max);
+  lowElement.innerHTML = Math.round(response.data.main.temp_min);
+  feelsElement.innerHTML = Math.round(response.data.main.feels_like);
+}
 
+let apiKey = "20ac730f4e6b3e4ccfed8e3f88036646";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Durham&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemprature);
