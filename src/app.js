8703e15c6d37176143fdd2f211f15c3c516d4d4a
Beas007 <98432650+Beas007@users.noreply.github.com>
2022-05-08 23:52:46 +0100

    added date

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${hours}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemprature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -8,6 +31,7 @@ function displayTemprature(response) {
   let highElement = document.querySelector("#high");
   let lowElement = document.querySelector("#low");
   let feelsElement = document.querySelector("#feels");
+  let dateElement = document.querySelector("#day");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
@@ -16,6 +40,7 @@ function displayTemprature(response) {
   highElement.innerHTML = Math.round(response.data.main.temp_max);
   lowElement.innerHTML = Math.round(response.data.main.temp_min);
   feelsElement.innerHTML = Math.round(response.data.main.feels_like);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "20ac730f4e6b3e4ccfed8e3f88036646";
